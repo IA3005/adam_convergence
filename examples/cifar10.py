84242ea932d74e88ddac8852207810756081a665
IA3005 <81386632+IA3005@users.noreply.github.com>
2025-05-11 01:35:10 +0200

    Update cifar10.py

--- examples/cifar10.py
+++ examples/cifar10.py
@@ -46,7 +46,7 @@ class Net(nn.Module):
     
 
 
-if __main__:
+if  __name__ == "__main__":
     
     criterion = nn.CrossEntropyLoss()
     
@@ -80,14 +80,14 @@ if __main__:
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2,
+                                              shuffle=True, num_workers=0,
                                               worker_init_fn = seed_worker_)
                                             
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
     
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2,
+                                         shuffle=False, num_workers=0,
                                          worker_init_fn = seed_worker_)
                                      
     res = {}
